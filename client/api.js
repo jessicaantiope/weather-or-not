c1bc665b06b542b0554216c199ae72c094ccf572
Your Name <you@example.com>
2018-02-09 11:02:01 +1300

    Connect to API

--- client/api.js
+++ client/api.js
@@ -1,3 +1,32 @@
 import request from 'superagent'
 
-const baseUrl =
+import CityList from './CityList'
+
+const baseUrl = "https://api.darksky.net/forecast/"
+const apiKey = '10b4534db4f46ed7adf2f97c71816d5d'
+// const coordinates = CityList.coordinates
+
+export function getWeather (callback, coords) {
+  request
+    .get(baseUrl + apiKey + '/' + coords)
+    .end((err, res) => {
+      callback(err, res.body)
+    })
+}
+
+/*Wellington
+41.2865,174.7762
+
+Hochiminh
+10.8231,106.6297
+
+Toronto
+10.8231,106.6297
+
+Sydney
+33.8688,151.2093
+
+Pangkor
+4.2275,100.5577
+
+*/
--- client/components/CityList.jsx
+++ client/components/CityList.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-export default ({/*argument*/}) => {
-  return (
-    <div>
-    </div>
-  )
+import WeatherDetails from './WeatherDetails'
+
+export default class CityList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      detailsVisible: false
+      // states got here
+    }
+    // binds go here
+  }
+  toggleVisibility() {
+    set.State({detailsVisible: true})
+  }
+  // functions go here
+  render() {
+    return (
+      <div>
+
+      {this.state.detailsVisible && <WeatherDetails />}
+      
+      </div>
+    )
+  }
 }
