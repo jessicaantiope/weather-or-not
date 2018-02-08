693cfd18d081af38f7ef214920f5e90ca307091c
kahu2018 <fake@fake.com>
2018-02-08 18:39:35 +1300

    Filled in templates for components

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+const routes = require('./routes/routes')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors({ origin: 'http://localhost:8080' }))
+app.use(express.static(path.join(__dirname, '../public')))
+app.use('/', routes)
+
+module.exports = app
