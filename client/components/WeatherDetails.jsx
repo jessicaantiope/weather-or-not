5631717f57b5f7c0849377e1ec9015dd82a13288
kahu2018 <fake@fake.com>
2018-02-09 15:34:25 +1300

    css works

--- client/components/WeatherDetails.jsx
+++ client/components/WeatherDetails.jsx
@@ -12,26 +12,28 @@ render () {
       <h1>{this.props.icon}</h1>
       <img src={getImageLinkOf(this.props.icon)}></img>
       <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-        <tr>
-          <td>Summary</td>
-          <td>{this.props.summary}</td>
-        </tr>
-        <tr>
-          <td>Probability</td>
-          <td>{this.props.precipProbability}</td>
-        </tr>
-        <tr>
-          <td>Temperature</td>
-          <td>{this.props.temperature} Fahrenheit</td>
-        </tr>
-        <tr>
-          <td>Humidity</td>
-          <td>{Math.floor(this.props.humidity * 100)}%</td>
-        </tr>
-        <tr>
-          <td>windspeed</td>
-          <td>{this.props.windSpeed}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Summary</td>
+            <td>{this.props.summary}</td>
+          </tr>
+          <tr>
+            <td>Probability</td>
+            <td>{this.props.precipProbability}</td>
+          </tr>
+          <tr>
+            <td>Temperature</td>
+            <td>{this.props.temperature} Fahrenheit</td>
+          </tr>
+          <tr>
+            <td>Humidity</td>
+            <td>{Math.floor(this.props.humidity * 100)}%</td>
+          </tr>
+          <tr>
+            <td>windspeed</td>
+            <td>{this.props.windSpeed}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
